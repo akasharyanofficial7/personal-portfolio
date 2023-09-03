608e3d27c3680432af1e28be871ef1edc9f5be7d
akasharyanofficial7 <akasharyangupta7@gmail.com>
2023-09-04 02:40:15 +0530

    commit1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import './about.css';
-import ME from  '../../assets/me1.png'
-import {  VscFolderActive  } from 'react-icons/vsc';
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me1.png";
+import { VscFolderActive } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me-image">
-            <img src={ME} alt="About Image"/>
+            <img src={ME} alt="About Image" />
           </div>
         </div>
         <div className="about_content">
           <div className="about_cards">
-            <article className='about_card'>
-            <VscFolderActive className='about_icon'/>
+            <article className="about_card">
+              <VscFolderActive className="about_icon" />
               <h5>Projects</h5>
               <small>4+ Completed </small>
             </article>
           </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque e maiores nihil ducimus at rem ullam reprehenderit quidem quia des amet repellat molestias quos totam. </p>
-               <a href="#contact" className='btn btn-primary'> Let's Talk </a>
+          <p
+            style={{
+              fontFamily: "Arial, sans-serif",
+              fontSize: "18px",
+              color: "#808e9b",
+            }}
+          >
+            I, Akash Gupta, am a MERN stack developer. I have a strong passion
+            for coding and designing. Completing IT projects and bringing
+            creative ideas to life is not just my profession but also my
+            enthusiasm.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            {" "}
+            Let's Talk{" "}
+          </a>
         </div>
       </div>
     </section>
